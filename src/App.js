3c48b2af19a487d544c8fb471eccf3374b5617d7
mafufan <dhgmlwo98@gmail.com>
2022-05-23 00:10:05 +0900

    홈페이지 상세페이지 컴포넌트 (미완성)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 /*eslint-disable*/
 import { useState } from 'react';
-import {
-  Button,
-  Navbar,
-  Container,
-  Nav,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   let [shoes] = useState(data);
@@ -17,38 +11,43 @@ function App() {
     <div className="App">
       <Navbar variant="dark" className="Navbar">
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Navbar.Brand href="/">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Info</Nav.Link>
+            <Nav.Link href="/">홈</Nav.Link>
+            <Nav.Link href="/detail">상세페이지</Nav.Link>
             <Nav.Link href="#pricing">Product</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <Routes>
+        <Route path="/" element={<Main shoes={shoes} />} />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+      </Routes>
+    </div>
+  );
+}
+
+function Main(props) {
+  return (
+    <>
       <div className="main-bg"></div>
       <Container>
         <Row>
-          {shoes.map(function (a, i) {
-            return <Card shoes={shoes[i]} i={i + 1} />;
+          {props.shoes.map(function (a, i) {
+            return <Card key={i} shoes={props.shoes[i]} i={i + 1} />;
           })}
         </Row>
       </Container>
       <Button variant="primary">Primary</Button>
-    </div>
+    </>
   );
 }
 
 function Card(props) {
   return (
-    <Col>
-      <img
-        src={
-          'https://codingapple1.github.io/shop/shoes' +
-          props.i +
-          '.jpg'
-        }
-        width="80%"
-      />
+    <Col key={props.i}>
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </Col>
