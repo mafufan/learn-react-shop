1a369d5cf5ceaa5b68e09ff660cbeca797f28235
huijae <mafufan@naver.com>
2022-05-22 16:03:13 +0900

    이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+/*eslint-disable*/
+import {
+  Button,
+  Navbar,
+  Container,
+  Nav,
+  Row,
+  Col,
+} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar variant="dark" className="Navbar">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Info</Nav.Link>
+            <Nav.Link href="#pricing">Product</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+        </Row>
+      </Container>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
