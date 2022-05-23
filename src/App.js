ec24522cbae39a8f236202561b37b5d7c9fb451f
mafufan <dhgmlwo98@gmail.com>
2022-05-23 15:41:21 +0900

    navigate, nested Routes, outlet

--- src/App.js
+++ src/App.js
@@ -3,54 +3,98 @@ import { useState } from 'react';
 import { Button, Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.js';
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
+
   return (
     <div className="App">
       <Navbar variant="dark" className="Navbar">
         <Container>
           <Navbar.Brand href="/">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">홈</Nav.Link>
-            <Nav.Link href="/detail">상세페이지</Nav.Link>
-            <Nav.Link href="#pricing">Product</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/detail');
+              }}
+            >
+              상세페이지
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/cart');
+              }}
+            >
+              Cart
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate('/about');
+              }}
+            >
+              About
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path="/" element={<Main shoes={shoes} />} />
-        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Container>
+                <Row>
+                  {shoes.map(function (a, i) {
+                    return <Card a={a} shoes={shoes[i]} i={i + 1} />;
+                  })}
+                </Row>
+              </Container>
+              <Button variant="primary">Primary</Button>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버들</div>} />
+          <Route path="location" element={<div>회사위치</div>} />
+        </Route>
+
+        <Route path="*" element={<div>없는 페이지요</div>} />
       </Routes>
     </div>
   );
 }
-
-function Main(props) {
+function About() {
   return (
     <>
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {props.shoes.map(function (a, i) {
-            return <Card key={i} shoes={props.shoes[i]} i={i + 1} />;
-          })}
-        </Row>
-      </Container>
-      <Button variant="primary">Primary</Button>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
     </>
   );
 }
 
 function Card(props) {
   return (
-    <Col key={props.i}>
-      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
-      <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.price}</p>
-    </Col>
+    <>
+      <Col key={props.a}>
+        {console.log(props.a)}
+        <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+        <h4>{props.shoes.title}</h4>
+        <p>{props.shoes.price}</p>
+      </Col>
+    </>
   );
 }
 
